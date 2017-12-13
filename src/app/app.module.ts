d47a26a0d6cb0f9b2039eb31be594ef54072b211
Ravi Chandran <chandrangreatfree@gmail.com>
2017-12-13 18:30:43 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
